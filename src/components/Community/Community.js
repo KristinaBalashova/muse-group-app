631ffbfa501538b067c4291e90773873542846f9
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-06-12 23:10:34 +0200

    Update semantic tags

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -6,12 +6,12 @@ export default function Community() {
     <section className={styles.root}>
       <div className={styles.container}>
         <SocialMediaLinks />
-        <div className={styles.text}>
+        <p className={styles.text}>
           You're not just a student - you're part of a vibrant community of like-minded individuals
           united by a passion for frontend development. Collaborate, innovate, and grow alongside
           fellow learners and industry professionals as you embark on this exciting journey
           together.
-        </div>
+        </p>
       </div>
     </section>
   );
