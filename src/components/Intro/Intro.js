b356ec0f724032416b54c99c04ae298e0e9f6ddb
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-06-12 22:40:43 +0200

    Fix paddings

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -6,22 +6,22 @@ export default function Intro() {
     <section className={styles.root}>
       <div className={styles.container}>
         <h1 className={styles.headline}>Muse Group Frontend Academy</h1>
-        <div className={styles.text}>
+        <p className={styles.subtitle}>
           Our academy offers a transformative learning experience designed to empower you with the
           skills and knowledge needed to succeed in the ever-evolving field of frontend development.
-        </div>
+        </p>
         <picture>
-          <source media="(max-width: 744px)" srcset="./images/musician-mobile.png" />
-          <source media="(min-width: 1024px)" srcset="./images/musician-desktop.png" />
+          <source media="(max-width: 744px)" srcSet="./images/musician-mobile.png" />
+          <source media="(min-width: 1024px)" srcSet="./images/musician-desktop.png" />
           <img
             src="./images/musician-desktop.png"
             alt="A guy playing guitar"
             className={styles.img}
           />
         </picture>
-        <div className={styles.text}>
+        <p className={styles.text}>
           Are you ready to turn your passion for technology into a thriving career?
-        </div>
+        </p>
         <EmailForm />
       </div>
     </section>
