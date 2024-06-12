1ecf398074d0c0625ce7b70d7132988aaa3c9af7
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-06-12 14:01:44 +0200

    Add Social Media Links

---
+++ src/components/SocialMediaLinks/SocialMediaLinks.js
@@ -0,0 +1,36 @@
+import styles from './SocialMediaLinks.module.css';
+
+const socialMediaList = [
+  {
+    id: 'insta',
+    href: 'https://www.instagram.com/animaapp/',
+  },
+  {
+    id: 'telegram',
+    href: 'https://twitter.com/AnimaApp',
+  },
+  {
+    id: 'linkedin',
+    href: 'https://www.linkedin.com/company/anima-app',
+  },
+  {
+    id: 'facebook',
+    href: 'https://discord.gg/eQxkYTNxSp',
+  },
+];
+
+//wrong links for telegram and facebook
+
+export function SocialMediaLinks() {
+  return (
+    <div className={styles.root}>
+      <div className={styles.socialList}>
+        {socialMediaList.map(({ id, href }) => (
+          <a href={href} target="_blank" rel="noreferrer noopener" className={styles.item} key={id}>
+            <img src={`/images/${id}.svg`} alt={`${id}-logo`} />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
