5c2ef918455a355007f8d52f121037b27de2fa6d
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-06-12 22:40:02 +0200

    Fix button size

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,14 +4,9 @@ import styles from './Button.module.css';
 //button styling options:
 //filled and text
 
-export const Button = ({ href = null, content, filled = false, text = false, type }) => (
-  <button
-    href={href}
-    className={cx(styles.button, filled && styles.filled, text && styles.text)}
-    type={type}
-  >
+export const Button = ({ href = null, content, filled = false, type }) => (
+  <button href={href} className={cx(styles.button, filled && styles.filled)} type={type}>
     {content}
-    {filled && <img src={`./images/arrow-big.svg`} alt={'arrow'} />}
-    {text && <img src={`./images/arrow-simple.svg`} alt={'arrow'} />}
+    <img src={`./images/arrow-big.svg`} alt={'arrow'} />
   </button>
 );
