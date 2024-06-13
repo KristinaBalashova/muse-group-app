6f8ee11735028cb99c871f10413a2df4b5a34a14
Kristina Balashova <infernalmajesty1350@gmail.com>
2024-06-13 10:15:16 +0200

    Reorganised css and made it prettier

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,6 @@
 import cx from 'classnames';
 import styles from './Button.module.css';
 
-//button styling options:
-//filled and text
-
 export const Button = ({ href = null, content, filled = false, type }) => (
   <button href={href} className={cx(styles.button, filled && styles.filled)} type={type}>
     {content}
